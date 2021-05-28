9aff4ea0bf5601a6ccb68f8c8d09b35305583712
francis1641 <francis1641@users.noreply.github.com>
2021-05-28 12:52:31 +0200

    Update index

--- index.js
+++ index.js
@@ -4,70 +4,34 @@ var app = new Vue({
   el: '#app',
   data: {
     info: null,
+    Autor:"",
+    Contenido:"",
+    Descripcion:"",
+    Publicado:"",
+    Enlace:"",
+    mostrar:false,
+    displayModal: 'none'
   },
   mounted () {
     axios
       .get('https://newsapi.org/v2/everything?q=game&pageSize=15&apiKey=5e976ef6430f49e08d17ea33c7605f41')
-      .then(response => ( this.info=response.data.articles));
+      .then(response => this.info=response.data.articles);
   },
   
   methods:{
-mostrarDetalle(articulo)
+mostrarDetalle(evento, articulo)
  {
-    modal = document.getElementById("modalDetalle");
-    if(articulo.author==null)
-      modal.children[0].children[0].children[1].innerHTML= "No hay autor";
-    else 
-      modal.children[0].children[0].children[1].innerHTML= articulo.author;
-    
-      if(articulo.content==null)
-        modal.children[0].children[0].children[2].innerHTML= "No hay contenido";
-      else 
-        modal.children[0].children[0].children[2].innerHTML= articulo.content;
-    
-      if(articulo.description==null)
-        modal.children[0].children[0].children[2].innerHTML= "No hay descripcion";
-      else 
-        modal.children[0].children[0].children[2].innerHTML= articulo.description;
-      modal.children[0].children[0].children[3].innerHTML= articulo.description;
-      modal.children[0].children[0].children[4].innerHTML= articulo.publishedAt;
-      modal.children[0].children[0].children[5].href= articulo.url;
-  
-    //modal.innerHTML = articulo.author;
-    modal.style.display = "block";
+  modal = document.getElementById("modalDetalle");
+    this.displayModal = 'block';
+    this.Autor = articulo.author ? articulo.author: "No hay autor";
+    this.Contenido = articulo.content ? articulo.content: "No hay contenido";
+    this.Descripcion = articulo.description ? articulo.description: "No hay descripcion";
+    this.Publicado = articulo.publishedAt;
+    this.Enlace = articulo.url;
+    //modal.style.display = "block";
+
 
  }
   }
 })
-
- function mostrarDetalle(articulo)
- {
-    modal = document.getElementById("modalDetalle");
-    if(articulo.author==null)
-      modal.children[0].children[0].children[1].innerHTML= "No hay autor";
-    else 
-      modal.children[0].children[0].children[1].innerHTML= articulo.author;
-    
-      if(articulo.content==null)
-        modal.children[0].children[0].children[2].innerHTML= "No hay contenido";
-      else 
-        modal.children[0].children[0].children[2].innerHTML= articulo.content;
-    
-      if(articulo.description==null)
-        modal.children[0].children[0].children[2].innerHTML= "No hay descripcion";
-      else 
-        modal.children[0].children[0].children[2].innerHTML= articulo.description;
-      modal.children[0].children[0].children[3].innerHTML= articulo.description;
-      modal.children[0].children[0].children[4].innerHTML= articulo.publishedAt;
-      modal.children[0].children[0].children[5].href= articulo.url;
-  
-    //modal.innerHTML = articulo.author;
-    modal.style.display = "block";
-
- }
-/*
- window.onload = () =>{
-   loadDoc("https://newsapi.org/v2/everything?q=game&pageSize=15&apiKey=5e976ef6430f49e08d17ea33c7605f41");
-   
- }*/
 
